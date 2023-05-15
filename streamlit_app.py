dece9b5d44ff3e393b500c28688240e6772ca551
Karanoza582002 <121099158+Karanoza582002@users.noreply.github.com>
2023-05-15 15:13:43 +0530

    Update streamlit_app.py
    Fixed Typos.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+
+streamlit.title('Hard-Boiled Free Ranged Egg')
